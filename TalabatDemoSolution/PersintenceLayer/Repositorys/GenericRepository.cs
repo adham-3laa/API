111821243744936ab7d280444562aa1761feeae7
adham-3la2 <adhamalaafouad_sd2024@fcis.bsu.edu.eg>
2025-11-06 17:14:26 +0200

    C44-G02-API03

---
+++ TalabatDemoSolution/PersintenceLayer/Repositorys/GenericRepository.cs
@@ -0,0 +1,44 @@
+﻿using DomainLayer.Contracts;
+using DomainLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using PersintenceLayer.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersintenceLayer.Repositorys
+{
+    public class GenericRepository<TEntity, TKey>(StoreDbContext _dbContext)
+        : IGenericRepository<TEntity, TKey>
+        where TEntity : BaseEntity<TKey>
+    {
+        public async Task AddAsync(TEntity entity)=>
+           await _dbContext.Set<TEntity>().AddAsync(entity);
+        
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()=>
+            await _dbContext.Set<TEntity>().ToListAsync();
+
+        public Task<IEnumerable<TEntity>> GetAllAsync(ISpecifications<TEntity, TKey> specifications)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<TEntity?> GetByIdAsync(TKey id)=>
+            await _dbContext.Set<TEntity>().FindAsync(id);
+
+        public Task<TEntity?> GetByIdAsync(ISpecifications<TEntity, TKey> specifications)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(TEntity entity)=>
+            _dbContext.Set<TEntity>().Remove(entity);
+       
+
+        public void Update(TEntity entity)
+       => _dbContext.Set<TEntity>().Update(entity);
+    }
+}
---
+++ TalabatDemoSolution/ServiceLayer/Specifications/BaseSpecifications.cs
@@ -0,0 +1,29 @@
+﻿using DomainLayer.Contracts;
+using DomainLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.Specifications
+{
+    public abstract class BaseSpecifications<TEntity, TKey> : ISpecifications<TEntity, TKey>
+    where TEntity : BaseEntity<TKey>
+    {
+        protected BaseSpecifications(Expression<Func<TEntity, bool>> criteria)
+        {
+            Criteria = criteria;
+        }
+
+        public Expression<Func<TEntity, bool>> Criteria { get; private set; }
+
+        public List<Expression<Func<TEntity, object>>> IncludeExpressions { get; } = [];
+        protected void AddInclude(Expression<Func<TEntity, object>> IncludeException)
+        {
+            IncludeExpressions.Add(IncludeException);
+        }
+            
+    }
+}
